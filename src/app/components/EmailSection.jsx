0556ab46396cc4551fd783c3044c2837745608df
will <gilbertozakarias@gmail.com>
2024-06-07 15:54:29 +0700

    fixed bug

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -9,7 +9,7 @@ const EmailSection = () => {
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative' id='#contact'>
         <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-500 to-transparent rounded-full h-80 w-80 z-0 blur-xl absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2'></div>
         <div>
-            <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+            <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>
                 {''}
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sed recusandae quisquam dolorum, placeat officia. Sit, corporis nemo? Numquam, delectus?
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
         <div className='grid grid-cols-1 sm:grid-cols-12'>
             <motion.div initial={{opacity: 0, scale: 0.5}} animate={{opacity: 1, scale: 1}} transition={{duration: 0.5}} className='col-span-8 place-self-center text-center sm:text-left justify-self-start'>
                 <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-4xl lg:leading-normal font-extrabold'>
-                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-primary-600 to-primary-300'>Hi, I'm {" "}
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-primary-600 to-primary-300'>Hi, I&apos;m {" "}
                     <br />
                     </span>
                     <TypeAnimation
